42eaf700ffd24388c0f8a1f678a5f627fce8c66c
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-10-04 14:06:48 -0400

    added user form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'application#home'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
