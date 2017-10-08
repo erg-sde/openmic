4556538410175c7c7061d4653d24aab4bb8d5f88
Alfonso Canales <canales101@yahoo.com>
2017-10-08 19:16:40 -0400

    did styling for the home page some back end issues need to be fixed with links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.js
 //= require turbolinks
 //= require_tree .
