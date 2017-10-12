32c2aa59df36a3243770c949b86bc34438af4ecf
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-10-12 18:03:57 -0400

    cleaned up a few conteztual behaviours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,13 @@ class UsersController < ApplicationController
     @event = Event.find(params[:event_id])
     @distance =  @event.venue.distance_to( [params[:lat].to_f, params[:long].to_f])
     @userevent = UserEvent.where(["user_id = ? and event_id = ?", session[:user_id], @event.id])
-    if !@userevent.nil? && @distance < 1
+    if !@userevent.nil? && @distance > 0
       @userevent.update(arrival: Time.now)
       flash[:success] = "a value was returned"
-      redirect_back fallback_location: root_path
+      redirect_back fallback_location: current_user
     else
       flash[:danger] = 'Looks like you have yet to arrive'
-      redirect_back fallback_location: root_path
+      redirect_back fallback_location: current_user
     end
   end
 
